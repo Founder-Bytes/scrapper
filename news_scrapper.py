310bd5b41216360206af2da4e2bb58eaa7556814
Denson Abraham <denson.a@crowdanalytix.net>
2022-02-20 10:28:50 +0530

    summary scraping + description scraping

--- news_scrapper.py
+++ news_scrapper.py
@@ -1,62 +1,91 @@
 import os
 import re
 import json
+from prometheus_client import Summary
 from selenium import webdriver
-from bs4 import BeautifulSoup 
+from bs4 import BeautifulSoup
 from selenium.webdriver.chrome.options import Options
 import pandas as pd
 import sys
 from selenium.webdriver.chrome.service import Service
 from dotenv import load_dotenv
+
+
 load_dotenv()
 options = Options()
 options.headless = True
 service = Service(os.getenv("CHROME_DRIVER"))
-print(os.getenv("CHROME_DRIVER")) 
-options.add_argument("--log-level=3")    
-driver = webdriver.Chrome(
-    service=service, options=options) 
+print(os.getenv("CHROME_DRIVER"))
+options.add_argument("--log-level=3")
+driver = webdriver.Chrome(service=service, options=options)
 
-links = []
-titles = [] 
-blogs = []
+
+def summary_formatter(summary):
+    joined_summary = "|".join(str(e) for e in summary)
+    return joined_summary
+
+
 def content_scraping(link):
-    page = f"{link}"  
+    blog_summary = []
+    page = f"{link}"
     driver.get(page)
     content = driver.page_source
-    soup = BeautifulSoup(content, features='html5lib')   
-    for para in soup.find_all('div',class_='entry-content clearfix'): 
+    soup = BeautifulSoup(content, features="html5lib")
+    for para in soup.find_all("div", class_="entry-content clearfix"):
         text = para.get_text()
-        break 
+        break
     text = text.rsplit(".")
-    text.pop(-1) 
-    blog = ''.join(str(e) for e in text)  
-    return blog 
+    text.pop(-1)
+    blog = ".".join(str(e) for e in text)
+    summary_lines = soup.find("div", class_="single-post-summary")
+    for lines in summary_lines.find_all("p"):
+        lines = lines.get_text()
+        blog_summary.append(lines)
+    return blog, blog_summary
 
 
-def home_page_scrapper():   
-    page = f"https://inc42.com/buzz/"
+def home_page_scrapper(count):
+    links = []
+    titles = []
+    blogs = []
+    summaries = []
+    page = f"https://inc42.com/buzz/page/{count}"
     driver.get(page)
     content = driver.page_source
-    soup = BeautifulSoup(content, features='html5lib') 
-    news_cards = soup.find_all('div',class_='card-content') 
-    for cards in news_cards: 
-        headline = cards.h2.text 
-        headline = headline.strip('\n')
-        link = cards.h2.a['href']  
-        blog = content_scraping(link)   
-        blog = blog.strip()
+    soup = BeautifulSoup(content, features="html5lib")
+    news_cards = soup.find_all("div", class_="card-content")
+    for cards in news_cards:
+        headline = cards.h2.text
+        headline = headline.strip("\n")
+        link = cards.h2.a["href"]
+        blog, blog_summary = content_scraping(link)
+        blog = blog.replace("\n", "")
+        blog = blog.replace("\xa0", " ")
         links.append(link)
-        titles.append(headline) 
-        blogs.append(blog) 
-    rows = pd.DataFrame({'Title':titles,'Link':links,'Blog':blogs}) 
-    rows.to_csv("scrapper/inc42/blogs.csv", index=False)
-def main(): 
-    home_page_scrapper()
+        titles.append(headline)
+        blogs.append(blog)
+        summary = summary_formatter(blog_summary)
+        summaries.append(summary)
+        # print(blog)
+    return titles, links, blogs, summaries
+
+
+def main():
+    count = 27
+    while count < 1500:
+        titles, links, blogs, summaries = home_page_scrapper(count)
+        # print(titles)
+        # print(links)
+        # print(blogs)
+        # print(summaries)
+        rows = pd.DataFrame(
+            {"Title": titles, "Link": links, "Blog": blogs, "Summary": summaries}
+        )
+        # rows = pd.DataFrame({'Title':titles,'Link':links,'Summary':summaries})
+        rows.to_csv("inc42/blogs_new.csv", index=False, mode="a", header=False)
+        print("page ", count)
+        count += 1
 
 
 if __name__ == "__main__":
     main()
-
-
-
--- search_space_scraper.py
+++ search_space_scraper.py
@@ -10,14 +10,14 @@ import pandas as pd
 import sys
 from selenium.webdriver.chrome.service import Service
 from dotenv import load_dotenv
+
 load_dotenv()
 options = Options()
-options.headless = True 
+options.headless = True
 options.add_argument("--log-level=3")
 service = Service(os.getenv("CHROME_DRIVER"))
 print(os.getenv("CHROME_DRIVER"))
-driver = webdriver.Chrome(
-    service=service, options=options)
+driver = webdriver.Chrome(service=service, options=options)
 
 
 def startup_india_scraping():
@@ -32,14 +32,16 @@ def startup_india_scraping():
             print(page)
             driver.get(page)
             content = driver.page_source
-            soup = BeautifulSoup(content, features='html5lib')
+            soup = BeautifulSoup(content, features="html5lib")
             startup_cards = soup.find_all(
-                'div', class_='category-card search-card new-eco-card')
+                "div", class_="category-card search-card new-eco-card"
+            )
             for cards in startup_cards:
                 name = cards.h3.text
                 names.append(name)
-                link = cards.find('a', href=re.compile(
-                    r'[/]([a-z]|[A-Z])\w+')).attrs['href']
+                link = cards.find("a", href=re.compile(r"[/]([a-z]|[A-Z])\w+")).attrs[
+                    "href"
+                ]
                 link = "https://www.startupindia.gov.in" + link
                 links.append(link)
                 if name not in mapping.keys():
@@ -48,10 +50,10 @@ def startup_india_scraping():
         except:
             pass
 
-    with open('data/startup_india/links.json', 'w', encoding='utf-8') as f:
+    with open("data/startup_india/links.json", "w", encoding="utf-8") as f:
         json.dump(mapping, f, ensure_ascii=False, indent=4)
 
-    rows = pd.DataFrame({'Name': names, 'Link': links})
+    rows = pd.DataFrame({"Name": names, "Link": links})
     rows.to_csv("data/startup_india/test_links.csv", index=False)
 
 
@@ -62,53 +64,53 @@ def startup1000():
     page = f"https://10000startups.com/our-startups"
     driver.get(page)
     content = driver.page_source
-    soup = BeautifulSoup(content, features='html5lib')
-    startup_cards = soup.find(
-        'div', class_='container startup-d')
-    list_of_cities = startup_cards.find_all('ul')
+    soup = BeautifulSoup(content, features="html5lib")
+    startup_cards = soup.find("div", class_="container startup-d")
+    list_of_cities = startup_cards.find_all("ul")
     for cities in list_of_cities:
-        list_of_startups = cities.find_all('li')
+        list_of_startups = cities.find_all("li")
         for startups in list_of_startups:
             city.append(cities.span.text)
             name.append(startups.h3.text)
-            links.append(startups.a['href'])
-    rows = pd.DataFrame({'Name': name, 'Link': links, 'City': city})
+            links.append(startups.a["href"])
+    rows = pd.DataFrame({"Name": name, "Link": links, "City": city})
     rows.to_csv("data/10000_startups/info.csv", index=False)
 
-def industry_filter_scrape():  
-    industry = [] 
+
+def industry_filter_scrape():
+    industry = []
     values = []
     page = f"https://www.startupindia.gov.in/content/sih/en/search.html?roles=Startup&page=0"
     driver.get(page)
     content = driver.page_source
-    soup = BeautifulSoup(content, features='html5lib')   
-    div_sector = soup.find('div', class_='mCSB_container') 
-    list_of_cards = div_sector.find_all('li') 
-    for cards in list_of_cards: 
-        input = cards.find('input') 
-        value = input.get('value') 
-        industry.append(cards.find('span').text) 
+    soup = BeautifulSoup(content, features="html5lib")
+    div_sector = soup.find("div", class_="mCSB_container")
+    list_of_cards = div_sector.find_all("li")
+    for cards in list_of_cards:
+        input = cards.find("input")
+        value = input.get("value")
+        industry.append(cards.find("span").text)
         values.append(value)
-    rows = pd.DataFrame({'industry_name': industry, 'value' : values})
+    rows = pd.DataFrame({"industry_name": industry, "value": values})
     rows.to_csv("startup_india/industry_links.csv", index=False)
 
-def sector_filter_scrape(): 
-    print("sector scraping") 
-    df = pd.read_csv ('startup_india/industry_links.csv')
-    for index, row in df.iterrows():  
-        industry_value = row['value'] 
-        page = f"https://www.startupindia.gov.in/content/sih/en/search.html?industries={industry_value}&roles=Startup&page=0" 
+
+def sector_filter_scrape():
+    print("sector scraping")
+    df = pd.read_csv("startup_india/industry_links.csv")
+    for index, row in df.iterrows():
+        industry_value = row["value"]
+        page = f"https://www.startupindia.gov.in/content/sih/en/search.html?industries={industry_value}&roles=Startup&page=0"
         print(page)
         driver.get(page)
-        content = driver.page_source 
-        soup = BeautifulSoup(content, features='html5lib')     
-        div_sector = soup.find('div', class_='filter-new') 
+        content = driver.page_source
+        soup = BeautifulSoup(content, features="html5lib")
+        div_sector = soup.find("div", class_="filter-new")
         print(div_sector)
         # print(soup)
-        # div_sector = soup.find('div', _class='accordion-section acc-2') 
-        val = soup.find('div', id_="mCSB_2_container")
+        # div_sector = soup.find('div', _class='accordion-section acc-2')
+        val = soup.find("div", id_="mCSB_2_container")
         print(val)
-
 
 
 def main():
@@ -116,7 +118,7 @@ def main():
     if scrapingsite == "startup-india":
         startup_india_scraping()
     elif scrapingsite == "1000startups":
-        startup1000() 
+        startup1000()
     elif scrapingsite == "industry_scrape":
         industry_filter_scrape()
     elif scrapingsite == "sector_scraping":
